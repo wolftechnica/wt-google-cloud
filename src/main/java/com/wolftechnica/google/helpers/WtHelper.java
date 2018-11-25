e7339d677e5330601639ff7ab2f11499100db816
Wolftechnica <>
2018-11-25 12:21:48 +0530

    image quality change

--- src/main/java/com/wolftechnica/google/helpers/WtHelper.java
+++ src/main/java/com/wolftechnica/google/helpers/WtHelper.java
@@ -1,6 +1,9 @@
 package com.wolftechnica.google.helpers;
 
+import java.awt.Dimension;
 import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.Transparency;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
@@ -18,21 +21,26 @@ import com.wolftechnica.google.exceptions.WtCloudExceptionCodes;
 import com.wolftechnica.google.service.core.GCSCoreService;
 
 /**
- * Class {@link WtHelper} provides the various utility that can make your integration easy.
+ * Class {@link WtHelper} provides the various utility that can make your
+ * integration easy.
+ * 
  * @author Wolftechnica
  *
  */
 public class WtHelper {
 
 	private static final Logger LOG = Logger.getLogger(GCSCoreService.class.getName());
- 
+
 	private WtHelper() throws IllegalAccessException {
 		throw new IllegalAccessException("Static helper.");
 	}
-	
+
 	/**
 	 * create a random string value that can be used for various purpose.
-	 * @param length : is the length of the the string value you that you need to generate 
+	 * 
+	 * @param length
+	 *            : is the length of the the string value you that you need to
+	 *            generate
 	 * @return random string
 	 */
 	public static String generateRandomString(int length) {
@@ -43,9 +51,6 @@ public class WtHelper {
 		}
 		return builder.toString();
 	}
-
-	
-
 
 	/**
 	 * 
@@ -67,48 +72,90 @@ public class WtHelper {
 	}
 
 	/**
-	 * @param file : byte array of file that needs to be resized
-	 * @param height : actual height in which file will be resized
-	 * @param width : actual width in which file will be resized
-	 * @param fileName : generated file name
+	 * @param file
+	 *            : byte array of file that needs to be resized
+	 * @param height
+	 *            : actual height in which file will be resized
+	 * @param width
+	 *            : actual width in which file will be resized
+	 * @param fileName
+	 *            : generated file name
 	 * @return byte of the generated file.
-	 * @throws IOException 
+	 * @throws IOException
 	 * @throws WtCloudException
 	 * 
 	 * 
-	 *  	<br>
+	 *             <br>
 	 *             75x75 - Gallery thumbnail (filename-small.jpg)<br>
 	 *             150x150 - Gallery thumbnail retina (filename-smallx2.jpg)<br>
 	 *             480x320 - Full image iPhone (filename-medium.jpg)<br>
 	 *             1024x768 - Full image Retina (filename-large.jpg)<br>
 	 * 
 	 */
-	public static byte[] resizeImage(byte[] file, int width, int height, String imageType) throws IOException  {
+	public static byte[] resizeImage(byte[] file, int width, int height, String imageType) throws IOException {
 		InputStream originalImage = new ByteArrayInputStream(file);
 		BufferedImage bImageFromConvert = ImageIO.read(originalImage);
-		int type = bImageFromConvert.getType() == 0 ? BufferedImage.TYPE_INT_ARGB : bImageFromConvert.getType();
-		BufferedImage resizedImage = new BufferedImage(width, height, type);
-		Graphics2D g = resizedImage.createGraphics();
-		g.drawImage(bImageFromConvert, 0, 0, width, height, null);
-		g.dispose();
+		BufferedImage resizedImage = getScaledInstance(bImageFromConvert, width, height,
+				RenderingHints.VALUE_INTERPOLATION_BILINEAR, true);
 		return convertBufferedImageToBYte(resizedImage, imageType);
 
 	}
-	
-	
-	private static byte[] convertBufferedImageToBYte(BufferedImage originalImage,String imageType) throws IOException{
+
+	private static byte[] convertBufferedImageToBYte(BufferedImage originalImage, String imageType) throws IOException {
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		ImageIO.write( originalImage, imageType, baos );
+		ImageIO.write(originalImage, imageType, baos);
 		baos.flush();
 		byte[] imageInByte = baos.toByteArray();
 		baos.close();
 		return imageInByte;
 	}
-	
+
+	public static BufferedImage getScaledInstance(BufferedImage img, int targetWidth, int targetHeight, Object hint,
+			boolean higherQuality) {
+		int type = (img.getTransparency() == Transparency.OPAQUE) ? BufferedImage.TYPE_INT_RGB
+				: BufferedImage.TYPE_INT_ARGB;
+		BufferedImage ret = img;
+		int w, h;
+		if (higherQuality) {
+			// Use multi-step technique: start with original size, then
+			// scale down in multiple passes with drawImage()
+			// until the target size is reached
+			w = img.getWidth();
+			h = img.getHeight();
+		} else {
+			// Use one-step technique: scale directly from original
+			// size to target size with a single drawImage() call
+			w = targetWidth;
+			h = targetHeight;
+		}
+
+		do {
+			if (higherQuality && w > targetWidth) {
+				w /= 2;
+				if (w < targetWidth) {
+					w = targetWidth;
+				}
+			}
+
+			if (higherQuality && h > targetHeight) {
+				h /= 2;
+				if (h < targetHeight) {
+					h = targetHeight;
+				}
+			}
+
+			BufferedImage tmp = new BufferedImage(w, h, type);
+			Graphics2D g2 = tmp.createGraphics();
+			g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, hint);
+			g2.drawImage(ret, 0, 0, w, h, null);
+			g2.dispose();
+
+			ret = tmp;
+		} while (w != targetWidth || h != targetHeight);
+
+		return ret;
+	}
+
 	private static final String ALPHA_NUMERIC_STRING = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
-	 
-
-	
- 
 }
--- src/test/java/com/test/demo.java
+++ src/test/java/com/test/demo.java
@@ -2,13 +2,12 @@ package com.test;
 
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 
-import com.wolftechnica.google.constants.FileExtenion;
-import com.wolftechnica.google.dto.WtBlob;
-import com.wolftechnica.google.dto.WtBlobId;
 import com.wolftechnica.google.exceptions.WtCloudException;
+import com.wolftechnica.google.helpers.WtHelper;
 import com.wolftechnica.google.service.GoogleClouldStorageService;
 
 public class demo {
@@ -29,8 +28,9 @@ public class demo {
 	 
 	public static void main(String[] args) throws WtCloudException, IOException {
 		//GCSCoreService coreService = GCSCoreService.getGCSCoreService(apiKey, projectId)
-		File f = new File("C:\\Users\\a1r1q88i\\Desktop\\certificateBG.jpg");
-		//new FileInputStream(f).
+/*		File f = new File("C:\\Users\\a1r1q88i\\Desktop\\static\\certificateBG.jpg");
+		byte[] fileContent = Files.readAllBytes(f.toPath());
+*/		/*//new FileInputStream(f).
 		byte[] fileContent = Files.readAllBytes(f.toPath());
 
 		
@@ -42,7 +42,24 @@ public class demo {
 		
 		WtBlob fileFromBucket = clouldStorageService.getFileFromBucket(uploadFileInFolder);
 		
-		System.out.println(fileFromBucket.getMediaLink());
-		 
+		System.out.println(fileFromBucket.getMediaLink());*/
+
+		
+		File f = new File("C:\\Users\\a1r1q88i\\Desktop\\static\\cr_841x595.jpg");
+		byte[] fileContent = Files.readAllBytes(f.toPath());
+		byte[] res = WtHelper.resizeImage(fileContent, 450, 450, "jpeg");
+		try (FileOutputStream fos = new FileOutputStream("C:\\Users\\a1r1q88i\\Desktop\\static\\cr_450x450.jpg")) {
+			   fos.write(res);
+		}
+		
+		f = new File("C:\\Users\\a1r1q88i\\Desktop\\static\\cr_841x595.jpg");
+		fileContent = Files.readAllBytes(f.toPath());
+		res = WtHelper.resizeImage(fileContent, 150, 150, "jpeg");
+		try (FileOutputStream fos = new FileOutputStream("C:\\Users\\a1r1q88i\\Desktop\\static\\cr_150x150.jpg")) {
+			   fos.write(res);
+		}
+		
+		
+		
 	}
 }
